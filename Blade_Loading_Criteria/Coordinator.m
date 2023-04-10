fbf329401c4c1ebe62c2b786b5775fe77f39914d
kspellas <kspellas96@gmail.com>
2023-04-10 15:11:14 +0200

    .

---
+++ Blade_Loading_Criteria/Coordinator.m
@@ -0,0 +1,42 @@
+%% Main Function of the Optimization.
+clear all
+close all
+clc
+
+
+%% Initial design vector
+
+x0 = [1,1,1,1];  % [phi,psi,solidity,maximum_thickness]
+
+%% Bounds of the optimization
+
+lb = [0.4,0.15,0.4,0.02];
+
+ub = [0.7,0.6 ,2 , 0.12];
+
+
+% Setting optimization options.
+options.Display         = 'iter-detailed';
+options.Algorithm       = 'sqp'; 
+options.FunValCheck     = 'off';
+options.DiffMinChange   = 5e-2;         % Minimum change while gradient searching
+options.DiffMaxChange   = 1;            % Maximum change while gradient searching
+options.TolCon          = 1e-3;         % Maximum difference between two subsequent constraint vectors [c and ceq]
+options.TolFun          = 1e-6;         % Maximum difference between two subsequent objective value
+options.TolX            = 1e-12;         % Maximum difference between two subsequent design vectors
+
+options.MaxIter         = 1000;         % Maximum iterations
+options.MaxFunEvals     = 4e3;          % Maximum number of function evaluations
+options.FinDiffType = 'Forward';
+
+options.PlotFcns = {@optimplotfval,@optimplotx,@optimplotfunccount,...
+                    @optimplotfirstorderopt,@optimplotstepsize,...
+                    @optimplotconstrviolation};    % Plotting the optimization parameters.
+
+
+% Running the optimization.  
+tic
+[x_opt,FVAL,exitflag,output] = fmincon(@Coordinator,x0,[],[],[],[],lb,ub,@Constraints,options);
+history.x = [history.x;x];
+toc
+
