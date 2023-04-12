9a671ae7a3c101b13398042bc70ee75fc5ef0aee
gsluisdom <91664912+gsluisdom@users.noreply.github.com>
2023-04-12 11:06:55 +0200

    tried to get a better first order opt

--- Blade_Loading_Criteria/Optimizer.m
+++ Blade_Loading_Criteria/Optimizer.m
@@ -19,15 +19,15 @@ ub = [0.7,0.7 ,2 , 0.12];
 options.Display         = 'iter-detailed';
 options.Algorithm       = 'sqp'; 
 options.FunValCheck     = 'off';
-options.DiffMinChange   = 5e-2;         % Minimum change while gradient searching
-options.DiffMaxChange   = 1;            % Maximum change while gradient searching
-options.TolCon          = 1e-3;         % Maximum difference between two subsequent constraint vectors [c and ceq]
+options.DiffMinChange   = 1e-5;         % Minimum change while gradient searching
+options.DiffMaxChange   = 1e-2;            % Maximum change while gradient searching
+options.TolCon          = 1e-12;         % Maximum difference between two subsequent constraint vectors [c and ceq]
 options.TolFun          = 1e-6;         % Maximum difference between two subsequent objective value
 options.TolX            = 1e-12;         % Maximum difference between two subsequent design vectors
 
 options.MaxIter         = 1000;         % Maximum iterations
 options.MaxFunEvals     = 4e3;          % Maximum number of function evaluations
-options.FinDiffType = 'Forward';
+options.FinDiffType = 'central';
 
 options.PlotFcns = {@optimplotfval,@optimplotx,@optimplotfunccount,...
                     @optimplotfirstorderopt,@optimplotstepsize,...
