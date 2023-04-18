535c84acc5b395a2ea6c0612ff3a2d7eef070e44
gsluisdom <91664912+gsluisdom@users.noreply.github.com>
2023-04-18 10:37:52 +0200

    normalizing the optmizier

--- Blade_Loading_Criteria/Constraints.m
+++ Blade_Loading_Criteria/Constraints.m
@@ -1,7 +1,12 @@
 function [c,ceq] = Constraints(x)
 %function that computes the inequality constraints
+global couplings
+    x0 = couplings.x0;
+    
+    x = x .* abs(x0);
+    
+ %% Set the design variables
 [phi,psi,sol_rt,max_th_rt,sol_st,max_th_st] =  deal(x(1),x(2),x(3),x(4),x(5),x(6));
-
 %% calculate velocity triangles and flow deflections
 a1 = 0 ;         % Inlet flow angle [degrees]
 
