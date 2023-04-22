8d37fe1e48155313b6ba89eff523305b4f4d53bd
kspellas <kspellas96@gmail.com>
2023-04-22 17:46:00 +0200

    Update Project_Initial.m

--- Project_Initial.m
+++ Project_Initial.m
@@ -16,7 +16,7 @@ M_1 = 0.8;                       % Mach Number at the inlet
 V_1 = M_1 * sqrt(k * R * T_1);   % Velocity at the inlet
 
 A_1 = m / (rho_1 * V_1);         % Area at the inlet of the fan
-Rt_1 = sqrt(A_1 / pi);           % Radius at the tip ( Assuming that Rh is small compared to Rt)
+
 
 n = 3342 * pi() / 30 ; % Rotational Speed [rad/s]
 
@@ -56,7 +56,7 @@ W_1 = Vm / cosd(b1) ;   % Relative Velocity at the inlet of the rotor [m/s]
 
 W_2 = Vm / cosd(b2) ;   % Relative Velocity at the outlet of the rotor [m/s]
 
-T_1 = Tt_1 - (0.5/Cp) * V_1^2;   % Static temperature at the inlet of the rotor [K]
+T_1_me = Tt_1 - (0.5/Cp) * V_1^2;   % Static temperature at the inlet of the rotor [K]!!!!!!
 T_2 = Tt_2 - (0.5/Cp) * V_2^2;   % Static temperature at the outlet of the rotor  [K]
 
 p_1 = pt_1 / (Tt_1 / T_1)^( k / ((k - 1) * n_p)); % Static pressire at the inlet of the rotor  [Pa]
@@ -65,7 +65,7 @@ p_2 = pt_2 / (Tt_2 / T_2)^( k / ((k - 1) * n_p)); % Static pressire at the outle
 rho_1 = p_1/(R_gas * T_1);           % Static pressure at the inlet of the rotor ,assuming IDEAL GAS [kg/m^3]
 rho_2 = p_2/(R_gas * T_2);           % Static pressure at the outlet of the rotor,assuming IDEAL GAS [kg/m^3]
 
-A_1 = m / (rho_1 * Vm);         % Area at the inlet of the rotor  [m^2]
+A_1_me = m / (rho_1 * Vm);         % Area at the inlet of the rotor  [m^2] !!!!!
 A_2 = m / (rho_2 * Vm);         % Area at the outlet of the rotor [m^2]
 
 H_1 = A_1 / (2 * pi() * r_mean);   % Blade Height of rotor  at the inlet[m]
@@ -78,7 +78,7 @@ r_tip_1 = H_1 + r_hub_1;             % Radius at the tip of the rotor (inlet) [m
 r_tip_2 = H_2 + r_hub_2;             % Radius at the tip of the stator (inlet)[m]
 
 
-M_1 =  V_1 / sqrt(k * R_gas * T_1); % Mach number at the inlet of the rotor [-]
+M_1_me =  V_1 / sqrt(k * R_gas * T_1); % Mach number at the inlet of the rotor [-] !!!!!
 M_2 =  V_2 / sqrt(k * R_gas * T_2); % Mach number at the outlet of the rotor [-]
 
 
