bc2c1fe7c06c44d078da35742deb77ee80973fad
kspellas <kspellas96@gmail.com>
2023-04-25 15:26:16 +0200

    Update Project_Initial.m

--- Project_Initial.m
+++ Project_Initial.m
@@ -25,12 +25,14 @@ b_tt = 1.4 ; % Design total to total pressure ratio [-]
 
 % Results from the fmincon
 
-%x_opt = !!!! 
+x_opt = [0.4576,0.3262,0.4,0.06,1.1810,0.06];
 [phi,psi,sol_rt,max_th_rt,sol_st,max_th_st] =  deal(x_opt(1),x_opt(2),x_opt(3),x_opt(4),x_opt(5),x_opt(6));
 
 
 
 %% Calculation for the inlet of the rotor( station 1)
+
+
 
 pt_1 = p_1 * ( 1 + ( (k - 1) / 2 ) * M_1^2 ) ^ (k / (k - 1)) ;      % Total pressure at the inlet [Pa]
 Tt_1 = T_1 * ( 1 + ( (k - 1) / 2 ) * M_1^2 ) ;                      % Total Temperature at the inlet [K]
@@ -43,9 +45,9 @@ pt_2 = pt_1 * b_tt;               % Total pressure at the outlet of the rotor [P
 
 w = Cp * ( Tt_2 - Tt_1);    % Specific work done by the fan [J/kg]
 
-U = sqrt(w / psi);   % Peripheral speed at the mean radius
+U_mean = sqrt(w / psi);   % Peripheral speed at the mean radius
 
-r_mean = U / n ;   % Mean radius ( also design radius)
+r_mean = U_mean / n ;   % Mean radius ( also design radius)
 
 Vm = phi * U_mean;  % Meridional (Axial) Velocity (constant throughout) [m/s]
 
@@ -64,6 +66,9 @@ b2 = atand( 1 / phi *( psi + phi * tand(a1) - 1));
 a2 = atand( tand(b2) + 1 / phi);
 
 a3 = a1 ; % assuming REPEATED STAGES
+
+n_p = 1; % Polytropic efficiency
+R_gas = 287; 
 
 
 V_1_me = Vm / cosd(a1) ;   % Absolute Velocity at the inlet of the rotor [m/s]   !!!!
@@ -122,5 +127,95 @@ N_st = (2 * pi() * r_mean) / pitch_st ; % Number of blades for the stator !!!!!
 %% Stability - Recovery Ratio
 
 R_R = ( cosd(b2)^2 * tand(b2) / phi ) - (cosd(a1)^2 * cosd(b2)^2 * tand(a1) * tand(b2) / phi^2) + (cosd(a1)^2 * tand(a1) / phi);
+
+
+%%%%%%%%%%%%%%%%%%%%% Incidence and deviation angles %%%%%%%%%%%%%%%%%%%%%%
+
+flow_defl_rt = b1 - b2; % Flow deflection calculated from the velocity triangles for the rotor
+
+flow_def_st = a2 - a3;  % Flow deflection calculated from the velocity triangles for the stator
+
+
+%% Howell's Loading Criterion for ROTOR
+
+db_star_rt = Howell_dbstar(abs(b2));  % Here we use the absolute value of b2
+phi_coeff_rt = Howell_Phi(Re);
+psi_coeff_rt = Howell_Psi(sol_rt);
+db_H_rt = Howell_delta_b(db_star_rt,phi_coeff_rt,psi_coeff_rt);
+
+%% Howell's Loading Criterion for STATOR
+db_star_st = Howell_dbstar(abs(a3));  % Here we use the absolute value of b2
+phi_coeff_st = Howell_Phi(Re);
+psi_coeff_st = Howell_Psi(sol_st);
+db_H_st = Howell_delta_b(db_star_st,phi_coeff_st,psi_coeff_st);
+
+
+%% Camber angle theta for ROTOR
+
+%%% Calculations for delta_0_rt 
+Kd_sh_rt     = Lieblein_Kd_sh(prof);
+Kd_t_rt      = Lieblein_K_delta(max_th_rt);
+d0_10_rt     = Lieblein_delta0_10(sol_rt,abs(b1));  % Here we use the absolute value of b1
+delta_0_rt   = Lieblein_delta_0(Kd_sh_rt,Kd_t_rt,d0_10_rt);  
+
+
+%%% Calculations for i_0_rt
+
+i0_10_rt = Lieblein_i0_10(sol_rt,abs(b1));    % Here we use the absolute value of b1
+Ki_t_rt  =  Lieblein_K_it(max_th_rt);
+i_0_rt   = Lieblein_i0(i0_10_rt,Ki_t_rt,prof);
+
+
+
+%%% Calculations for the rest of the parameters that theta_rt needs
+
+m_coeff_rt = Lieblein_M_coeff(abs(b1),prof);
+exp_b_rt = Lieblein_expo_b(abs(b1));
+n_coeff_rt = Lieblein_n_coeff(sol_rt,abs(b1));
+
+theta_rt = camber(b1,b2,delta_0_rt,i_0_rt,m_coeff_rt,sol_rt,exp_b_rt,n_coeff_rt);
+
+%% Camber angle theta for STATOR
+
+%%% Calculations for delta_0_st 
+Kd_sh_st     = Lieblein_Kd_sh(prof);
+Kd_t_st      = Lieblein_K_delta(max_th_st);
+d0_10_st     = Lieblein_delta0_10(sol_st,abs(a2));  % Here we use the absolute value of a2
+delta_0_st   = Lieblein_delta_0(Kd_sh_st,Kd_t_st,d0_10_st);  
+
+
+%%% Calculations for i_0_st
+
+i0_10_st = Lieblein_i0_10(sol_st,abs(a2));    % Here we use the absolute value of a2
+Ki_t_st  =  Lieblein_K_it(max_th_st);
+i_0_st   = Lieblein_i0(i0_10_st,Ki_t_st,prof);
+
+
+
+%%% Calculations for the rest of the parameters that theta_st needs
+
+m_coeff_st = Lieblein_M_coeff(abs(a2),prof);
+exp_b_st = Lieblein_expo_b(abs(a2));
+n_coeff_st = Lieblein_n_coeff(sol_st,abs(a2));
+
+theta_st = camber(a2,a3,delta_0_st,i_0_st,m_coeff_st,sol_st,exp_b_st,n_coeff_st);
+
+
+
+
+%% Incidence and Deviation angles for ROTOR
+
+in_angle_rt = Lieblein_inc(i_0_rt,n_coeff_rt,theta_rt);
+
+
+dev_angle_rt = Lieblein_deviation(delta_0_rt,m_coeff_rt,sol_rt,exp_b_rt,theta_rt);
+
+
+%% Incidence and Deviation angles for STATOR
+
+in_angle_st = Lieblein_inc(i_0_st,n_coeff_st,theta_st);
+
+
+dev_angle_st = Lieblein_deviation(delta_0_st,m_coeff_st,sol_st,exp_b_st,theta_st);
 
 
