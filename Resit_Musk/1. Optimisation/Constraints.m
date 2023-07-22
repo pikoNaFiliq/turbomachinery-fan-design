b7c986477a6b5573f71782ab0017870eeb3b12b8
gsluisdom <91664912+gsluisdom@users.noreply.github.com>
2023-07-22 16:22:00 +0200

    constraints added

---
+++ Resit_Musk/1. Optimisation/Constraints.m
@@ -0,0 +1,18 @@
+function [c,ceq] = Constraints(x)
+
+global couplings
+    x0 = couplings.x0;
+    
+    x = x .* abs(x0)
+    
+    %% Set the design variables
+    [phi,psi,power] =  deal(x(1),x(2),x(3));
+    
+    %% U blade constraints
+    
+mdot = 212; %kg
+n_stages = 1;
+a0 = 0: %degrees
+sp_power = power/(n_stages*mdot);
+
+U_blade = sqrt(sp_power/psi);  %U_blade based on the mean chord
